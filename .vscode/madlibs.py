d0903df76fca59848f6dd335c2d8e2b3bdf06e5d
RAJAVARSHINI C <rajavarshinic@gmail.com>
2023-04-28 17:10:15 +0530

    my first repository
    just something that i tried from youtube

---
+++ .vscode/madlibs.py
@@ -0,0 +1,14 @@
+# String concatenation
+#youtuber = "Raja"
+#print("Subscribe to "+youtuber)
+#print("Subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective:")
+verb1 = input("Verb:")
+verb2 = input("Verb2:")
+famous_person = input("Famous person:")
+madlib = f"Computer Programming is so {adj}! It makes me so exited all the time \
+ because I love to {verb1} Stay hydrated and {verb2} like you are a {famous_person}!"
+
+print(madlib)
